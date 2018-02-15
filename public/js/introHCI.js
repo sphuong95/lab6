f6a52123ad2bccc2ef843a2b9985621970799ecd
Shaquile Phuong <sphuong@ucsd.edu>
2018-02-15 01:20:44 -0800

    lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,18 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber, callBack); 
+}
+
+
+function callBack(result) {
+
+	var projectHTML = '<img src="' + result['image'] + '" class="detailsImage">' + 
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' + result['date'] + '</p>' + 
+		'<p>' + result['summary'] + '</small></p>';
+
+	var projectID = '#project' + result.id + '.details';
+
+	$(projectID).html(projectHTML);
 }
